15a7c77be81b37371e56f2cc83998cd77048064b
PENNYWICH <levitskiui@gmail.com>
2025-06-05 19:12:19 +0300

    Исправлены приватные атрибуты и проверки типов

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self._price = price  # Приватный атрибут
+        self.__price = price  # Полностью приватный атрибут
         self.quantity = quantity
 
     @property
     def price(self):
-        return self._price
+        return self.__price
 
     @price.setter
-    def price(self, new_price):
+    def price(self, new_price: float):
         if new_price > 0:
-            self._price = new_price
+            self.__price = new_price
         else:
             print("Цена не должна быть нулевая или отрицательная")
 
@@ -38,8 +38,11 @@ class Category:
         Category.product_count += len(products)
 
     def add_product(self, product: Product):
-        self.__products.append(product)
-        Category.product_count += 1
+        if isinstance(product, Product):  # Проверка типа
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError("Можно добавлять только объекты Product или его подклассы")
 
     @property
     def products(self):
@@ -51,38 +54,3 @@ class Category:
     @property
     def products_list(self):
         return self.__products
-
-
-if __name__ == "__main__":
-    # Тестовые данные
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    category1 = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3]
-    )
-
-    print(category1.products)
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products)
-    print(f"Общее количество продуктов: {Category.product_count}")
-
-    new_product = Product.new_product(
-        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера",
-         "price": 180000.0, "quantity": 5})
-    print(new_product.name)
-    print(new_product.description)
-    print(new_product.price)
-    print(new_product.quantity)
-
-    new_product.price = 800
-    print(new_product.price)
-
-    new_product.price = -100
-    print(new_product.price)
-    new_product.price = 0
-    print(new_product.price)
